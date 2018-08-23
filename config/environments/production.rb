9cbf31f9bc48926ffcdad05eb474e53091b16b63
Thomas <thomas@kactus.com>
2018-08-23 22:48:12 +0200

    configure environment for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.asset_host = ENV['ASSETS_HOST']
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
